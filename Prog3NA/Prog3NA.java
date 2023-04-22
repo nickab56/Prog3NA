53c886db3f84f6f3863f9cd032c48c481b7647c9
nickab56 <nick.abegg@yahoo.com>
2023-04-22 09:07:22 -0400

    updated and fixed up output

--- Prog3NA/Prog3NA.java
+++ Prog3NA/Prog3NA.java
@@ -61,44 +61,51 @@ public class Prog3NA
     
     public static boolean Start(String token)
     {
-        System.out.println("In Start");
+        System.out.println("Entering function: Start");
+        outStream.println("Entering function: Start");
         if (encodeFile(token))
             if (lineFormat(token = ReadToken()))
                 if (encodeFormat(token = ReadToken()))
                     if (outputFile(token = ReadToken()))
                     {
-                    System.out.println("Start-success");
-                    outStream.println("Start-success");
+                    System.out.println("Leaving function: Start (Success)");
+                    outStream.println("Leaving function: Start (Success)");
                     return true;
                     }
-            return false;
+        System.out.println("Leaving function: Start (failed)");
+        outStream.println("Leaving function: Start (failed)"); 
+        return false;
     }
     
     public static boolean encodeFile(String token)
     {
-        System.out.println("In encodeFile");
+        System.out.println("Entering function: encodeFile");
+        outStream.println("Entering function: encodeFile");
         if (name(token = ReadToken()))
             if((token = ReadToken()).charAt(0) == '.')
                 if (name(token = ReadToken()))
                     if (stmtTerminator(token = ReadToken()))
                     {
-                    System.out.println("encodeFile-success");
-                    outStream.println("encodeFile-success");
+                    System.out.println("Leaving function: encodeFile (Success)");
+                    outStream.println("Leaving function: encodeFile (Success)");
                     return true;
                     }
+        System.out.println("Leaving function: encodeFile (failed)");
+        outStream.println("Leaving function: encodeFile (failed)");            
         return false;
     }
     
     public static boolean name(String token)
     {
-        System.out.println("In name");
+        System.out.println("Entering function: name");
+        outStream.println("Entering function: name");
         int i = token.length();
         if (i == 1)
         {
             if (letter(token.charAt(0)))
             {
-                System.out.println("name-success");
-                outStream.println("name-success");
+                System.out.println("Leaving function: name (Success)");
+                outStream.println("Leaving function: name (Success)");
                 return true;
             }
         }
@@ -110,94 +117,112 @@ public class Prog3NA
                     letter(token.charAt(x));
                     x++;
                 }
-                System.out.println("name-success");
-                outStream.println("name-success");
+                System.out.println("Leaving function: name (Success)");
+                outStream.println("Leaving function: name (Success)");
                 return true;
             }
+        System.out.println("Leaving function: name (failed)");
+        outStream.println("Leaving function: name (failed)");
         return false;
     }
     
     public static boolean lineFormat(String token)
     {
-        System.out.println("In lineFormat");
+        System.out.println("Entering function: lineFormat");
+        outStream.println("Entering function: lineFormat");
         if (token.equals("line_format"))
             if ((token = ReadToken()).equals("is"))
                 if (lineFormatType(token = ReadToken()))
                 {
                     if(stmtTerminator(tempToken))
                     {
-                        System.out.println("lineFormat-success");
-                        outStream.println("lineFormat-success");
+                        System.out.println("Leaving function: lineFormat (Success)");
+                        outStream.println("Leaving function: lineFormat (Success)");
                         return true;
                     }
                 }
+        System.out.println("Leaving function: lineFormat (failed)");
+        outStream.println("Leaving function: lineFormat (failed)");        
         return false;
     }
     
     public static boolean encodeFormat(String token)
     {
-        System.out.println("In encodeFormat");
+        System.out.println("Entering function: encodeFormat");
+        outStream.println("Entering function: encodeFormat");
         if (token.equals("encode_format"))
             if((token = ReadToken()).equals("is"))
                 if (encodeFormatType(token = ReadToken()))
                 {
                     if(stmtTerminator(tempToken))
                     {
-                        System.out.println("encodeFormat-success");
-                        outStream.println("encodeFormat-success");
+                        System.out.println("Leaving function: encodeFormat (Success)");
+                        outStream.println("Leaving function: encodeFormat (Success)");
                         return true;
                     }
                 }
+        System.out.println("Leaving function: encodeFormat (failed)");
+        outStream.println("Leaving function: encodeFormat (failed)");        
         return false;
     }
     
     public static boolean outputFile(String token)
     {
-        System.out.println("In outputFile");
+        System.out.println("Entering function: outputFile");
+        outStream.println("Entering function: outputFile");
         if (name(token = ReadToken())){
             if((token = ReadToken()).charAt(0) == '.')
                 if(name(token = ReadToken()))
                     if(stmtTerminator(token = ReadToken()))
                     {
-                        System.out.println("outputFile-success");
-                        outStream.println("outputFile-success");
+                        System.out.println("Leaving function: outputFile (Success)");
+                        outStream.println("Leaving function: outputFile (Success)");
                         return true;
                     }
                 }
+        System.out.println("Leaving function: outputFile (failed)");
+        outStream.println("Leaving function: outputFile (failed)");
         return false;
     }
     
     public static boolean stmtTerminator(String token)
     {
-        System.out.println("In stmtTerminator");    
+        System.out.println("Entering function: stmtTerminator");
+        outStream.println("Entering function: stmtTerminator");    
         if (token.charAt(0) == '!')
         {
-            System.out.println("stmtTerminator-success");
-            outStream.println("stmtTerminator-success");
+            System.out.println("Leaving function: stmtTerminator (Success)");
+            outStream.println("Leaving function: stmtTerminator (Success)");
             return true;
         }
+        System.out.println("Leaving function: stmtTerminator (failed)");
+        outStream.println("Leaving function: stmtTerminator (failed)");
         return false;
     }
     
     public static boolean lineFormatType(String token)
     {
-        System.out.println("In lineFormatType");
+        System.out.println("Entering function: lineFormatType");
+        outStream.println("Entering function: lineFormatType");
         if (token.equals("string") || token.equals("number"))
         {
             if (lineFormatTypePrime(token = ReadToken()))
             {
-                System.out.println("lineFormatType-success");
-                outStream.println("lineFormatType-success");
+                System.out.println("Leaving function: lineFormatType (Success)");
+                outStream.println("Leaving function: lineFormatType (Success)");
                 return true;
             }
         }
+        System.out.println("Leaving function: lineFormatType (failed)");
+        outStream.println("Leaving function: lineFormatType (failed)");
         return false;
     }
     
     
     public static boolean lineFormatTypePrime(String token)
     {
-        System.out.println("In lineFormatTypePrime");
+        System.out.println("Entering function: lineFormatTypePrime");
+        outStream.println("Entering function: lineFormatTypePrime");
         tempToken = token;
         if (token.charAt(0) == ',')
         {
@@ -206,25 +231,28 @@ public class Prog3NA
             {
                 if(lineFormatTypePrime(token = ReadToken()))
                 {
-                    System.out.println("lineFormatTypePrime-success");
-                    outStream.println("lineFormatTypePrime-success");
+                    System.out.println("Leaving function: lineFormatTypePrime (Success)");
+                    outStream.println("Leaving function: lineFormatTypePrime (Success)");
                     return true;
                 }
             }
         }
+        System.out.println("Leaving function: lineFormatTypePrime (failed)");
+        outStream.println("Leaving function: lineFormatTypePrime (failed)");
         return true;
     }
     
     
     public static boolean encodeFormatType(String token)
     {
-        System.out.println("In encodeFormatType");
+        System.out.println("Entering function: encodeFormatType");
+        outStream.println("Entering function: encodeFormatType");
         if (token.equals("none"))
         {
             if (encodeFormatTypePrime(token = ReadToken()))
                 {
-                    System.out.println("encodeFormatType-success");
-                    outStream.println("encodeFormatType-success");
+                    System.out.println("Leaving function: encodeFormatType (Success)");
+                    outStream.println("Leaving function: encodeFormatType (Success)");
                     return true;   
                 }
         }
@@ -235,17 +263,21 @@ public class Prog3NA
             {
                 if (encodeFormatTypePrime(token = ReadToken()))
                 {
-                    System.out.println("encodeFormatType-success");
-                    outStream.println("encodeFormatType-success");
+                    System.out.println("Leaving function: encodeFormatType (Success)");
+                    outStream.println("Leaving function: encodeFormatType (Success)");
                     return true;
                 }
             }
         }
+        System.out.println("Leaving function: encodeFormatType (failed)");
+        outStream.println("Leaving function: encodeFormatType (failed)");
         return false;
     }
     
     public static boolean encodeFormatTypePrime(String token)
     {
+        System.out.println("Entering function: encodeFormatTypePrime");
+        outStream.println("Entering function: encodeFormatTypePrime");
         tempToken = token;
         if (token.charAt(0) == ',')
             {
@@ -256,26 +288,29 @@ public class Prog3NA
                     {
                         if (encodeFormatTypePrime(token = ReadToken()))
                         {
-                            System.out.println("encodeFormatTypePrime-success");
-                            outStream.println("encodeFormatTypePrime-success");
+                            System.out.println("Leaving function: encodeFormatTypePrime (Success)");
+                            outStream.println("Leaving function: encodeFormatTypePrime (Success)");
                             return true;
                         }
                     }
                 }
             }
+        System.out.println("Leaving function: encodeFormatTypePrime (failed)");
+        outStream.println("Leaving function: encodeFormatTypePrime (failed)");
         return true;
     }
     
     public static boolean digits(String token)
     {
-        System.out.println("In digits");
+        System.out.println("Entering function: digits");
+        outStream.println("Entering function: digits");
         int i = token.length();
         if (i == 1)
         {
             if (digit(token.charAt(0)))
             {
-                System.out.println("digits-success");
-                outStream.println("digits-success");
+                System.out.println("Leaving function: digits (Success)");
+                outStream.println("Leaving function: digits (Success)");
                 return true;
             }
         }
@@ -287,36 +322,51 @@ public class Prog3NA
                     digit((token.charAt(x)));
                     x++;
                 }
+                System.out.println("Leaving function: digits (Success)");
+                outStream.println("Leaving function: digits (Success)");
                 return true;
             }
+        System.out.println("Leaving function: digits (failed)");
+        outStream.println("Leaving function: digits (failed)");
         return false;
     }
     
     public static boolean letter(char tokenChar)
     {
-        System.out.println("In letter");
+        System.out.println("Entering function: letter");
+        outStream.println("Entering function: letter");
         if (Character.isAlphabetic(tokenChar))
         {
-            System.out.println("letter-success");
-            outStream.println("letter-success");
+            System.out.println("Leaving function: letter (Success)");
+            outStream.println("Leaving function: letter (Success)");
             return true;
         }
         else
+        {
+            System.out.println("Leaving function: letter (failed)");
+            outStream.println("Leaving function: letter (failed)");
             return false;
+        }
     }
     
     public static boolean digit(char tokenChar)
     {
-        System.out.println("In digit");
+        System.out.println("Entering function: digit");
+        outStream.println("Entering function: digit");
         int tokenInt = Character.getNumericValue(tokenChar);
         if (tokenInt >= 0)
         {
-            System.out.println("digit-success");
-            outStream.println("digit-success");
+            System.out.println("Leaving function: digit (Success)");
+            outStream.println("Leaving function: digit (Success)");
             return true;
         }
         else
+        {
+            System.out.println("Leaving function: digit (failed)");
+            outStream.println("Leaving function: digit (failed)");
             return false;
+        }
+
     }
     
     
